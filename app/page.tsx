92fdd80af34517e90fb45c94f8fcd99dc1bc00c0
sachingadhari <37650415+GadhariSachin@users.noreply.github.com>
2024-04-18 20:21:26 +0530

    Initialized Next app and clean up

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <h1 className="text-xl">Interview Preparation - Machine Coding round</h1>
+    </main>
+  );
+}
